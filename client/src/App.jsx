ff93ecd8b8e46e3d971f2cb2e8c1b83300e871d8
kanishk.v <kanishk.v@yanoljacloudsolution.com>
2025-01-13 10:17:55 +0530

    implementing the emit, disconnect, connect and broadcast

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,36 @@
-import React from 'react'
+//import { useEffect } from 'react'
+import React, {useEffect} from 'react'
 import {io} from 'socket.io-client'
+import {Container, Typography} from '@mui/material'
 
 const App = () => {
 
 
   //just because the two urls are different then it is blocked by cors
   const socket = io("http://localhost:3000")
+
+  useEffect(() => {
+      socket.on("connect", () => {
+        console.log("connected", socket.id)
+      })
+      //here welcome is the event which is in the app.js and the data to be passed is called using an object which is s
+      socket.on("welcome", (s) => {
+        console.log(s)
+      })
+
+      return () => {
+        socket.disconnect()
+      }
+  }, [])
+  
   
 
   return (
-    <div>App</div>
+    <Container>
+      <Typography>
+        welcome to socket io
+      </Typography>
+    </Container>
   )
 }
 
--- server/app.js
+++ server/app.js
@@ -19,8 +19,25 @@ const io = new Server(server, {
 //now we want to listen hence we use on and if we wanted to send some data then we use emit
 //here we are using on means the server will listen
 io.on("connection", (socket) => {
-    console.log("user connected")
-    console.log("Id",socket.id)
+    console.log("user connected", socket.id)
+
+    //the below line will send a message to a particular user
+    socket.emit("welcome", `welcome to the server, ${socket.id}`);
+
+    //the below line will broadcast a message to every user 
+    socket.broadcast.emit("welcome", `${socket.id} joined the server`)
+
+    //the below statement means when we get a "message" from the frontend we will console log it
+    socket.on("message", (data) => {
+        console.log(data);
+        io.emit("message",data)
+    })
+
+    //the below line will show how to disconnect a user
+    //now in order to disconnect the user will be unmounted and we need to pass a cleanup function in the useEfect
+    socket.on("disconnect", () => {
+        console.log("user disconnected", socket.id)
+    })
 })
 
 app.use(cors({
